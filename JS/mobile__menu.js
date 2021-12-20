0cccb7b6e346b5a88e9ea52d05e85a1a5130189d
Abeille <tabeille@albmolecular.com>
2021-12-20 14:22:37 +0100

    Fix responsive problems 5

--- JS/mobile__menu.js
+++ JS/mobile__menu.js
@@ -5,6 +5,8 @@ $(function() {
         {
             $('.section__2__container').fadeOut(50);
             $('.section__1__container').fadeOut(50);
+            $('.section__3__container').fadeOut(50);
+            $('.section__4__container').fadeOut(50);
             $('.mobile__menu').fadeIn(300);
             $('.mobile__menu').css("display", "flex");
             
@@ -15,7 +17,16 @@ $(function() {
             $('.mobile__menu').fadeOut(50);
             $('.section__1__container').fadeIn(200);
             $('.section__2__container').fadeIn(200);
+            $('.section__3__container').fadeIn(200);
+            $('.section__4__container').fadeIn(200);
             return false;
         }
+    });
+    $('.mobile__menu a').click(function(){
+        $('.mobile__menu').fadeOut(50);
+        $('.section__1__container').fadeIn(200);
+        $('.section__2__container').fadeIn(200);
+        $('.section__3__container').fadeIn(200);
+        $('.section__4__container').fadeIn(200);
     });
 });
